6d7ef4eb0ecca85c877eeaf9c77aec210cb1986f
quertuy <quertuy13@hotmail.com>
2024-03-21 09:37:01 -0500

    ventana victoria need fix

---
+++ Assets/Juego/Script/PopUp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+
+    public GameObject ventanaActivar;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Comprobar si el objeto que hizo contacto tiene la etiqueta "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Activar la ventana
+            ventanaActivar.SetActive(true);
+        }
+
+    }
+    }
